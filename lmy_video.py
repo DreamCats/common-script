54e120ea0d8cafe9d5d3950ab3bd7cf06d19a5cf
DreamCats <maifeng_cat@outlook.com>
2019-02-10 22:26:39 +0800

    测试获取视频信息方法

--- lmy_video.py
+++ lmy_video.py
@@ -9,17 +9,19 @@ class Config:
     username = '17608037124'
     password = 'maifeng868'
     # 课程id
-    course_id = [
+    # course_id = [
         # '7D49841E-AE74-11E8-AA22-7CD30AD36C02', # 英语id
         # '8D8A9E92-F137-11E8-832A-EC0D9ACEE976', # test_id
         # '03C0CDC4-F242-11E8-832A-EC0D9ACEE976', # 英语口语房
-    ]
+    # ]
     base_url = 'https://www.mosoteach.cn/web/index.php' # lmy web_url
     login_url = 'https://www.mosoteach.cn/web/index.php?c=passport&m=account_login'
     # 登录post参数
     login_datas  = {'account_name':username, 'user_pwd':password, 'remember_me': 'N'}
     # 主页
     index_url = 'https://www.mosoteach.cn/web/index.php?c=clazzcourse&m=index'
+    # 视频主页资源
+    video_url = 'https://www.mosoteach.cn/web/index.php?c=res&m=index&clazz_course_id={course_id}'
     
 
 class LmyVideo(object):
@@ -30,6 +32,8 @@ class LmyVideo(object):
     def start(self):
         self.login()
         self.get_index()
+        course_id = '03C0CDC4-F242-11E8-832A-EC0D9ACEE976'
+        self.get_video_resource(course_id)
        
 
     def login(self):
@@ -85,7 +89,48 @@ class LmyVideo(object):
         except Exception as e:
             print('get_index', e)
             
+    def get_video_resource(self, course_id):
+        '''获取视频主页资源
+        :param course_id: 课程id
+        :return: items_list:
+                    {
+                        'data_value',
+                        'video_time',
+                        'course_id',
+                    }
+        '''
+        index_url = self.config.video_url.format(course_id=course_id)
+        try:
+            res_video_index = self.s.get(url=index_url)
+            if res_video_index.status_code == 200:
+                doc = pq(res_video_index.text)
+                items = doc('.res-row-open-enable').items()
+                items_list = []
+                for item in items:
+                    if item.attr('data-mime') == 'video':
+                        data_value = item.attr('data-value')
+                        # print('视频id:',data_value)
+                        video_time = re.findall(r'.*?<span>(.*?) 分钟</span>.*?',str(item('.create-box')))[0]
+                        # print('视频时间:', video_time)
+                        items_list.append(
+                            {
+                                'data_value':data_value,
+                                'video_time':video_time,
+                                'course_id':course_id
+                            }
+                        )
+                        # self.get_parse(data_value, video_time, class_id)
+                    else:
+                        continue
+                return items_list
+            else:
+                return None
+        except Exception as e:
+            print('get_video_resource->', e)
 
+    def parse_video(self):
+        '''
+        '''
 
 def main():
     lmy_video = LmyVideo(Config)
