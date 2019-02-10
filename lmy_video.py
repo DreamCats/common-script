73b9942deea360ca017f40c577bea7d3cc391f62
DreamCats <maifeng_cat@outlook.com>
2019-02-10 19:02:21 +0800

    测试登录方法

--- lmy_video.py
+++ lmy_video.py
@@ -10,26 +10,59 @@ class Config:
     password = 'maifeng868'
     # 课程id
     course_id = [
-        '0EF18A8D-B4E8-11E8-AA22-7CD30AD36C02', # 毛中特id
         # '7D49841E-AE74-11E8-AA22-7CD30AD36C02', # 英语id
         # '8D8A9E92-F137-11E8-832A-EC0D9ACEE976', # test_id
         # '03C0CDC4-F242-11E8-832A-EC0D9ACEE976', # 英语口语房
     ]
-    headers = {
-        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-        'Host': 'www.mosoteach.cn',
-        'X-Requested-With':'XMLHttpRequest',
-        'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-        'Accept':'application/json, text/javascript, */*; q=0.01',
-    }
     base_url = 'https://www.mosoteach.cn/web/index.php' # lmy web_url
-    login_params = {'c':'passport', 'm':'account_login'} # 登录url参数
+    login_url = 'https://www.mosoteach.cn/web/index.php?c=passport&m=account_login'
     # 登录post参数
     login_datas  = {'account_name':username, 'user_pwd':password, 'remember_me': 'N'}
-    # 获取资源前的静态方法，获取params参数
-    @staticmethod
-    def get_resource_params(clazz_id):
-        return {'c':'res', 'm':'index', 'clazz_course_id': clazz_id}
-    # 秒看视频的url的参数
-    watch_params = {'c':'res','m':'save_watch_to'}
+    # 主页
+    index_url = 'https://www.mosoteach.cn/web/index.php?c=clazzcourse&m=index'
     
+
+class LmyVideo(object):
+    def __init__(self, config):
+        self.config = config
+        self.s  = requests.session()
+
+    def start(self):
+        self.login()
+       
+
+    def login(self):
+        '''登录方法
+        '''
+        try:
+            res_login = self.s.post(url=self.config.login_url, data=self.config.login_datas)
+            if res_login.status_code == 200:
+                if res_login.json()['result_msg'] == 'OK':
+                    print('登陆成功...')
+                    # print(res_login.json())
+                    return res_login.json()
+                else:
+                    print('登陆失败，账号密码错误...')
+            else:
+                return None
+        except Exception as e:
+            print('异常:login->', e)
+
+    def get_index(self):
+        '''获取主页
+        :return: 待定
+        '''
+        try:
+            pass
+        except Exception as e:
+            print('get_index', e)
+            
+
+
+def main():
+    lmy_video = LmyVideo(Config)
+    lmy_video.start()
+
+
+if __name__ == "__main__":
+    main()
