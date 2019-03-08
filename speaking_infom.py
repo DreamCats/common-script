577f3d806cfced9fa8c6321dc98b37093b7c3537
DreamCats <maifeng_cat@outlook.com>
2019-03-08 17:54:19 +0800

    添加配置信息

--- speaking_infom.py
+++ speaking_infom.py
@@ -3,3 +3,16 @@
 # __time__: 2019:03:08:17:50
 
 # config 配置信息
+
+class Config:
+    # 学号：
+    username = '201822090429' 
+    # 密码
+    password = '220031'
+    # 周期
+    periods = '180' # 暂定三分钟 
+    # 口语访次数
+    speaking_count = '2' # 这个可是要经常修改的
+
+
+
