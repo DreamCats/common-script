225c8ad71b5ac2e0b35f79b3a66d01f7c17f01f3
DreamCats <maifeng868@gmail.com>
2022-03-03 20:07:17 +0800

    feat: add utools-iterm2 plugin

---
+++ utools/iterm/preload.js
@@ -0,0 +1,277 @@
+function dateFormat(fmt, date) {
+    let ret;
+    const opt = {
+        "Y+": date.getFullYear().toString(),        // 年
+        "m+": (date.getMonth() + 1).toString(),     // 月
+        "d+": date.getDate().toString(),            // 日
+        "H+": date.getHours().toString(),           // 时
+        "M+": date.getMinutes().toString(),         // 分
+        "S+": date.getSeconds().toString()          // 秒
+        // 有其他格式化字符需求可以继续添加，必须转化成字符串
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length === 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        }
+    }
+    return fmt;
+}
+
+const saveOrUpdateDb = function (key, data) {
+    if (!data) {
+        return
+    }
+    let keyData = window.utools.db.get(key);
+    if (!keyData) {
+        window.utools.db.put({
+            _id: key,
+            data: data
+        })
+        return
+    }
+    utools.db.put({
+        _id: key,
+        data: data,
+        _rev: keyData._rev
+    })
+};
+
+const addHistory = item => {
+    if (!item) {
+        return
+    }
+    let title = item.title;
+    item.description = dateFormat("mm-dd HH:MM:SS", new Date())
+
+    let history = getHistory() || [];
+    let index = -1;
+    for (let i = 0; i < history.length; i++) {
+        if (history[i].title === title) {
+            index = i;
+        }
+    }
+    if (index !== -1) {
+        history.splice(index, 1);
+    }
+    history.unshift(item);
+    if (history.length > 50) {
+        history = history.slice(0, 50);
+    }
+    saveOrUpdateDb('history', history);
+};
+
+const getHistory = function () {
+    let history = window.utools.db.get("history");
+    if (history) {
+        return history.data
+    }
+    return history;
+}
+
+const handleSearch = function (searchWord, callbackSetList) {
+    let items = getHistory()||[];
+    if (searchWord) {
+        items = items.filter(item => item.title.indexOf(searchWord) !== -1);
+        items.unshift({
+            title: searchWord
+        });
+    }
+    if (items.length !== 0) {
+        callbackSetList(items)
+    }
+};
+
+const execCommand = function (itemData,path) {
+    if (!itemData || !itemData.title) {
+        return
+    }
+    let goPath = path ? 'tell current session to write text "cd ' + path + '"' : '';
+    require('child_process').exec(`osascript -e 'tell application "iTerm"
+        activate
+        try
+            
+            select current window
+            tell current window
+                create tab with default profile
+            end tell
+            tell current window
+                ${goPath}
+                tell current session to write text "${itemData.title}"
+            end tell
+        on error
+            create window with default profile
+            select current window
+            tell current window
+                ${goPath}
+                tell current session to write text "${itemData.title}"
+            end tell
+        end try
+    end tell'`, (error, stdout, stderr) => {
+        console.log('exec:',stdout)
+        console.log('exec:',error)
+        if (error) return window.utools.showNotification(stderr)
+        window.utools.outPlugin()
+    });
+    addHistory(itemData)
+};
+
+const execCommandKitty = function (itemData,path) {
+    if (!itemData || !itemData.title) {
+        return
+    }
+    let goPath = path ? 'tell current session to write text "cd ' + path + '"' : '';
+    require('child_process').exec(`osascript -e 'tell application "kitty"
+        activate
+        try
+            select current window
+            tell current window
+                create tab with default profile
+            end tell
+            tell current window
+                ${goPath}
+                tell current session to write text "${itemData.title}"
+            end tell
+        on error
+            create window with default profile
+            select current window
+            tell current window
+                ${goPath}
+                tell current session to write text "${itemData.title}"
+            end tell
+        end try
+    end tell'`, (error, stdout, stderr) => {
+        console.log('exec:',stdout)
+        console.log('exec:',error)
+        if (error) return window.utools.showNotification(stderr)
+        window.utools.outPlugin()
+    });
+    addHistory(itemData)
+};
+
+window.exports = {
+    "iterm_command": {
+        mode: "list",
+        args: {
+            // 进入插件时调用（可选）
+            enter: (action, callbackSetList) => {
+                if (action.type === 'over' && action.payload) {
+                    // 直接调用setSubInputValue不生效，特殊处理
+                    setTimeout(function () {
+                        window.utools.setSubInputValue(action.payload);
+                    }, 50);
+                } else {
+                    let history = getHistory();
+                    if (history) {
+                        callbackSetList(history)
+                    }
+                }
+
+            },
+            // 子输入框内容变化时被调用 可选 (未设置则无搜索)
+            search: (action, searchWord, callbackSetList) => {
+                handleSearch(searchWord, callbackSetList)
+            },
+            // 用户选择列表中某个条目时被调用
+            select: (action, itemData, callbackSetList) => {
+                window.utools.hideMainWindow()
+                execCommand(itemData)
+                window.utools.outPlugin()
+            },
+            placeholder: "回车执行"
+        }
+    },
+    "current_iterm_command": {
+        mode: "list",
+        args: {
+            // 进入插件时调用（可选）
+            enter: (action, callbackSetList) => {
+                if (action.type === 'over' && action.payload) {
+                    // 直接调用setSubInputValue不生效，特殊处理
+                    setTimeout(function () {
+                        window.utools.setSubInputValue(action.payload);
+                    }, 50);
+                } else {
+                    let history = getHistory();
+                    if (history) {
+                        callbackSetList(history)
+                    }
+                }
+
+            },
+            // 子输入框内容变化时被调用 可选 (未设置则无搜索)
+            search: (action, searchWord, callbackSetList) => {
+                handleSearch(searchWord, callbackSetList)
+            },
+            // 用户选择列表中某个条目时被调用
+            select: (action, itemData, callbackSetList) => {
+                window.utools.hideMainWindow()
+                execCommand(itemData,window.utools.getCurrentFolderPath() || '~')
+                window.utools.outPlugin()
+            },
+            placeholder: "回车当前目录执行"
+        }
+    },
+    "kitty_command": {
+        mode: "list",
+        args: {
+            // 进入插件时调用（可选）
+            enter: (action, callbackSetList) => {
+                if (action.type === 'over' && action.payload) {
+                    // 直接调用setSubInputValue不生效，特殊处理
+                    setTimeout(function () {
+                        window.utools.setSubInputValue(action.payload);
+                    }, 50);
+                } else {
+                    let history = getHistory();
+                    if (history) {
+                        callbackSetList(history)
+                    }
+                }
+
+            },
+            // 子输入框内容变化时被调用 可选 (未设置则无搜索)
+            search: (action, searchWord, callbackSetList) => {
+                handleSearch(searchWord, callbackSetList)
+            },
+            // 用户选择列表中某个条目时被调用
+            select: (action, itemData, callbackSetList) => {
+                window.utools.hideMainWindow()
+                execCommandKitty(itemData)
+                window.utools.outPlugin()
+            },
+            placeholder: "回车执行"
+        }
+    },
+    "current_kitty_command": {
+        mode: "list",
+        args: {
+            // 进入插件时调用（可选）
+            enter: (action, callbackSetList) => {
+                if (action.type === 'over' && action.payload) {
+                    // 直接调用setSubInputValue不生效，特殊处理
+                    setTimeout(function () {
+                        window.utools.setSubInputValue(action.payload);
+                    }, 50);
+                } else {
+                    let history = getHistory();
+                    if (history) {
+                        callbackSetList(history)
+                    }
+                }
+
+            },
+            // 子输入框内容变化时被调用 可选 (未设置则无搜索)
+            search: (action, searchWord, callbackSetList) => {
+                handleSearch(searchWord, callbackSetList)
+            },
+            // 用户选择列表中某个条目时被调用
+            select: (action, itemData, callbackSetList) => {
+                window.utools.hideMainWindow()
+                execCommandKitty(itemData,window.utools.getCurrentFolderPath() || '~')
+                window.utools.outPlugin()
+            },
+            placeholder: "回车当前目录执行"
+        }
+    }
+}
